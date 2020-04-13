da703dea95c162fa5a1cf52b8726adb149f0c478
ryan <rghunter@calpoly.edu>
2020-04-13 09:10:02 -0700

    added input support with form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
+
 class App extends Component {
     state = {
-        characters: [
-          {
-            name: 'Charlie',
-            job: 'Janitor',
-          },
-          {
-            name: 'Mac',
-            job: 'Bouncer',
-          },
-          {
-            name: 'Dee',
-            job: 'Aspring actress',
-          },
-          {
-            name: 'Dennis',
-            job: 'Bartender',
-          },
-        ]
+        characters: []
     }
     render() { 
         const { characters } = this.state
